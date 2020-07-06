3145015b5d00df294244bde44b8e53b3e6e34b71
Jake Mills <jake.mills.05@gmail.com>
2020-07-06 01:16:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,11 +4,9 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
     # binding.pry
     value.each do |color, names|
-      # binding.pry
       names.each do |name|
         binding.pry
       end
-      item
     end
     final_array
   end
